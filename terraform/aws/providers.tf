4de63d507e889e1e4251a80f1dc295e364f636c6
Giselle E <y1ngyang@users.noreply.github.com>
2024-03-27 13:41:22 -0600

    Update providers.tf

--- terraform/aws/providers.tf
+++ terraform/aws/providers.tf
@@ -7,8 +7,8 @@ provider "aws" {
 provider "aws" {
   alias      = "plain_text_access_keys_provider"
   region     = "us-west-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  access_key = "not here anymore"
+  secret_key = "not here anymore"
 }
 
 terraform {
